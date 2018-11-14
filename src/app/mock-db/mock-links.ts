aa25a6ba9d8303643616a7e957c80249ca0eb5ab
Xephy <adr3nalinexin@gmail.com>
2018-11-14 19:17:40 +0200

    Should be done this time

--- src/app/mock-db/mock-links.ts
+++ src/app/mock-db/mock-links.ts
@@ -2,16 +2,18 @@ import { Links } from "../shared/links.model";
 
 export const LINKS: Array<Links> = [
   { title: "homepage", router: "/", hasDropdown: false },
-  { title: "about", router: "", hasDropdown: true },
+  { title: "about", router: "/about-us", hasDropdown: false },
   { title: "gallery", router: "/gallery", hasDropdown: false },
-  { title: "games", router: "/games", hasDropdown: false },
-  { title: "blog", router: "/blog", hasDropdown: false },
-  { title: "contact us", router: "/contact-us", hasDropdown: false }
+  { title: "FAQ", router: "/faq", hasDropdown: false },
+  { title: "Confession Corner", router: "/confessions", hasDropdown: false },
+  // { title: "games", router: "/games", hasDropdown: false },
+  // { title: "blog", router: "/blog", hasDropdown: false },
+  // { title: "contact us", router: "/contact-us", hasDropdown: false }
 ];
 
 export const DROPDOWN_LINKS: Array<Links> = [
   { title: "About Us", router: "/about-us" },
   { title: "FAQ", router: "/faq" },
-  { title: "Team", router: "/about-us" },
+  // { title: "Team", router: "/about-us" },
   { title: "Confession Corner", router: "/confessions" }
 ];
