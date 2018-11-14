4d77fc5c98088689242b5c9a78172e5e85a64516
Xephy <adr3nalinexin@gmail.com>
2018-11-14 19:47:28 +0200

    Why

--- src/app/mock-db/mock-images.ts
+++ src/app/mock-db/mock-images.ts
@@ -1,20 +1,20 @@
 import { Image } from "../shared/images.model";
 
 export const IMAGES: Array<Image> = [
-  { imageUrl: '../../assets/images/585x550.jpg', alt: 'Man holding a game controller' },
-  { imageUrl: '../../assets/images/404-bg.jpg', alt: 'Space' },
-  { imageUrl: '../../assets/images/about-bg.jpg', alt: 'Arcade center' },
-  { imageUrl: '../../assets/images/blog-img-1.jpg', alt: 'Blog Image 1' },
-  { imageUrl: '../../assets/images/blog-img-2.jpg', alt: 'Blog Image 2' },
-  { imageUrl: '../../assets/images/blog-img-3.jpg', alt: 'Blog Image 3' },
-  { imageUrl: '../../assets/images/footer-bg.jpg', alt: 'Footer Background' },
-  { imageUrl: '../../assets/images/game-img-1.jpg', alt: 'Game Image 1' },
-  { imageUrl: '../../assets/images/game-img-2.jpg', alt: 'Game Image 2' },
-  { imageUrl: '../../assets/images/game-img-3.jpg', alt: 'Game Image 3' },
-  { imageUrl: '../../assets/images/game-img-4.jpg', alt: 'Game Image 4' },
-  { imageUrl: '../../assets/images/game-slide-img-1.jpg', alt: 'Game Slide image' },
-  { imageUrl: '../../assets/images/page-name.jpg', alt: 'Page Name' },
-  { imageUrl: '../../assets/images/slide-1.jpg', alt: 'Slide 1' },
-  { imageUrl: '../../assets/images/slide-2.jpg', alt: 'Slide 2' },
-  { imageUrl: '../../assets/images/slide-3.jpg', alt: 'Slide 3' }
+  { imageUrl: 'assets/images/585x550.jpg', alt: 'Man holding a game controller' },
+  { imageUrl: 'assets/images/404-bg.jpg', alt: 'Space' },
+  { imageUrl: 'assets/images/about-bg.jpg', alt: 'Arcade center' },
+  { imageUrl: 'assets/images/blog-img-1.jpg', alt: 'Blog Image 1' },
+  { imageUrl: 'assets/images/blog-img-2.jpg', alt: 'Blog Image 2' },
+  { imageUrl: 'assets/images/blog-img-3.jpg', alt: 'Blog Image 3' },
+  { imageUrl: 'assets/images/footer-bg.jpg', alt: 'Footer Background' },
+  { imageUrl: 'assets/images/game-img-1.jpg', alt: 'Game Image 1' },
+  { imageUrl: 'assets/images/game-img-2.jpg', alt: 'Game Image 2' },
+  { imageUrl: 'assets/images/game-img-3.jpg', alt: 'Game Image 3' },
+  { imageUrl: 'assets/images/game-img-4.jpg', alt: 'Game Image 4' },
+  { imageUrl: 'assets/images/game-slide-img-1.jpg', alt: 'Game Slide image' },
+  { imageUrl: 'assets/images/page-name.jpg', alt: 'Page Name' },
+  { imageUrl: 'assets/images/slide-1.jpg', alt: 'Slide 1' },
+  { imageUrl: 'assets/images/slide-2.jpg', alt: 'Slide 2' },
+  { imageUrl: 'assets/images/slide-3.jpg', alt: 'Slide 3' }
 ]
